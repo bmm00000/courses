72aa37e97101b2ce5b3fa8c8b25471138fdee791
Jose Boix <bmm00000@icloud.com>
2023-03-15 20:01:37 +0100

    add functionality to ListGroup component

---
+++ react1/first-vite-project/src/components/ListGroup.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+interface Props {
+  heading: string;
+  items: string[];
+}
+
+const ListGroup = ({ heading, items }: Props) => {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>There are no items</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={`list-group-item ${index === selectedIndex && "active"}`}
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ListGroup;
