2c7d58941e4376c429a6224aa555000df2e54f57
Jose Boix <bmm00000@icloud.com>
2023-04-11 19:24:03 +0200

    Add grid

--- react1/games-app/src/App.tsx
+++ react1/games-app/src/App.tsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav"
+      "main"`,
+        lg: `"nav nav"
+      "aside main"`,
+      }}
+    >
+      <GridItem pl="2" bg="pink.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem pl="2" bg="green.300" area={"aside"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem pl="2" bg="blue.300" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
