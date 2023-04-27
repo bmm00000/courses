8ae5d8fb0e9894a49a5df19c541235fe766d26f4
Jose Boix <bmm00000@icloud.com>
2023-04-27 19:32:46 +0200

    call onAddUser when form is submitted

--- rtl-jest/1stapp/src/components/__test__/UserForm.test.js
+++ rtl-jest/1stapp/src/components/__test__/UserForm.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "../../App";
+import UserForm from "../UserForm";
 
 test("should display two inputs and one button", () => {
   render(<App />);
@@ -9,4 +10,19 @@ test("should display two inputs and one button", () => {
   const button = screen.getByRole("button");
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
+});
+
+it("should call onAddUser when form is submitted", async () => {
+  const mockOnAddUser = jest.fn(() => {});
+  render(<UserForm onAddUser={mockOnAddUser} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  await userEvent.click(nameInput);
+  await userEvent.keyboard("John");
+  await userEvent.click(emailInput);
+  await userEvent.keyboard("john@gmail.com");
+  const submitButton = screen.getByRole("button", { name: /submit/i });
+  await userEvent.click(submitButton);
+
+  expect(mockOnAddUser).toHaveBeenCalled();
 });
