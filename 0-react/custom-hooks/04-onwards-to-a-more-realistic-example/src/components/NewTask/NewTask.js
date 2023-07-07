90800c69c5804635cf3e9ceace28e03347ed23e0
Jose Boix <bmm00000@icloud.com>
2023-07-07 17:58:53 +0200

    add custom useHttp hook

---
+++ 0-react/custom-hooks/04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
@@ -0,0 +1,36 @@
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: postTask } = useHttp();
+
+  const enterTaskHandler = (taskText) => {
+    const addTask = (taskData) => {
+      const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+      props.onAddTask(createdTask);
+    };
+    postTask(
+      {
+        url: "https://react-http-6b4a6.firebaseio.com/tasks.json",
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      addTask
+      // watch again 228 (Using the custom hook in more components) to see the other option using 'bind'.
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
